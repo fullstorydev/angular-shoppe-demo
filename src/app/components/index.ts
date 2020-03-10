2a70674a15532eada4db57032fcb11ec32c0456d
van-fs <van@fullstory.com>
2020-03-10 16:53:18 -0400

    create components barrel

---
+++ src/app/components/index.ts
@@ -0,0 +1,4 @@
+export * from './cart/cart.component';
+export * from './checkout/checkout.component';
+export * from './products/products.component';
+export * from './thank-you/thank-you.component';
