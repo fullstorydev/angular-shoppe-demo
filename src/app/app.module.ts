4109b7f7319d0cbf152fefe43893892f33b2ff80
van-fs <van@fullstory.com>
2020-03-10 16:53:54 -0400

    add components to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent, CheckoutComponent, ProductsComponent, ThankYouComponent } from './components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    CheckoutComponent,
+    ProductsComponent,
+    ThankYouComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    MatButtonModule,
+    MatIconModule,
     MatToolbarModule,
   ],
   providers: [],
