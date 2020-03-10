8465e4fce56acb62553db5624a38c8fc32e901b3
van-fs <van@fullstory.com>
2020-03-10 13:54:33 -0400

    organize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
-
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
