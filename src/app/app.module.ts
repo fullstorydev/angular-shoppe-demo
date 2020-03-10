5a5031d1da48a850da4a599874966b8dd38e933c
Van Staub <52075243+van-fs@users.noreply.github.com>
2020-03-10 13:58:03 -0400

    Create site layout structure (#4)
    * add angular material and flex-layout
    
    * create app skeleton
    
    * organize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
-
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
