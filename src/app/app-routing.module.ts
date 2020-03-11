c8418b476d781aa38c42a0346815329d676eb268
Van Staub <52075243+van-fs@users.noreply.github.com>
2020-03-11 07:53:15 -0400

    Initialize core components and routes (#5)
    * add cart component
    
    * add checkout component
    
    * add products component
    
    * add thankyou component
    
    * create components barrel
    
    * add components to app
    
    * update component routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent, CheckoutComponent, ProductsComponent, ThankYouComponent } from './components';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'thankyou', component: ThankYouComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styles: []
+})
+export class CartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
