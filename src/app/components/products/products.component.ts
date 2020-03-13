63281c1026c1129168aa0187af302d269ba4750a
van-fs <van@fullstory.com>
2020-03-13 11:05:10 -0400

    add products page

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
+import { ProductService } from 'src/app/services';
+import { Product } from 'src/app/models';
+
+/**
+ * Breakpoint aliases (e.g. 'sm') to number of grid-list columns.
+ */
+interface BreakpointMap {
+  [name: string]: number
+}
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styles: []
+  styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  // TODO (van) a better approach is min/max width based on the product's image size
+  private columnBreakpoints: BreakpointMap = {
+    'default': 3,
+    'xs': 1,
+    'sm': 2,
+    'md': 3,
+    'lg': 4,
+    'xl': 5,
+  };
+
+  numColumns = this.columnBreakpoints['default'];
+  products: Product[] = [];
+
+  constructor(
+    private mediaObserver: MediaObserver,
+    private productService: ProductService,
+  ) { }
 
   ngOnInit() {
+    this.observeBreakpoints();
+    this.buildProductList();
   }
 
+  /**
+   * Observes for breakpoint changes and updates the number of columns.
+   * See `columnBreakpoints` for breakpoint-to-columns mapping.
+   */
+  private observeBreakpoints(): void {
+    this.mediaObserver.media$.subscribe((change: MediaChange) => {
+      const n = this.columnBreakpoints[change.mqAlias];
+      this.numColumns = n > 0 ? n : this.columnBreakpoints['default'];
+    });
+  }
+
+  /**
+   * Builds a list of products from the catalog.
+   */
+  buildProductList(): void {
+    this.productService.getProducts().subscribe(products => this.products = products);
+  }
 }
