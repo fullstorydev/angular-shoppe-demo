2c5e984a536f7b3008f1b4692c80b49aa988c906
van-fs <van@fullstory.com>
2020-07-10 15:48:33 -0400

    add firebase and AngularFire authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -11,16 +13,20 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
+import { environment } from 'src/environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent, CartButtonComponent, CheckoutComponent, ProductsComponent, ThankYouComponent } from './components';
 import { CartService, ProductService } from './services';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -30,9 +36,12 @@ import { CartService, ProductService } from './services';
     CheckoutComponent,
     ProductsComponent,
     ThankYouComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
@@ -45,6 +54,7 @@ import { CartService, ProductService } from './services';
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatMenuModule,
     MatSelectModule,
     MatSnackBarModule,
     MatTableModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,6 +15,16 @@ export const environment = {
   remoteApiRoot: 'http://localhost:3000/api',
   useMockApi: true,
   orgId: '',
+  firebase: {
+    apiKey: "AIzaSyAIj9o7fb-2KwcN_ptxnZx7KgT_K1cPpO8",
+    authDomain: "ng-shoppe.firebaseapp.com",
+    databaseURL: "https://ng-shoppe.firebaseio.com",
+    projectId: "ng-shoppe",
+    storageBucket: "ng-shoppe.appspot.com",
+    messagingSenderId: "934374301881",
+    appId: "1:934374301881:web:850c4310ddc83d6e863b68",
+    measurementId: "G-WNKVY3ERLJ"
+  }
 };
 
 /*
