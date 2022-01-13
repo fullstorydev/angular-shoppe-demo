6545f441f30111b0b33632019ef280662b0e083b
PerStirpes <mccarl@asu.edu>
2022-01-13 18:13:44 -0500

    Add random generators

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,6 +1,21 @@
 import { HostListener, Component } from "@angular/core"
 import { MatDialog, MatDialogRef } from "@angular/material/dialog"
 import * as FullStory from "@fullstory/browser"
+import getEmoji from "get-random-emoji"
+import { generateEmail } from "email-generator"
+import { uniqueNamesGenerator, Config, adjectives, colors, animals } from "unique-names-generator"
+
+const customConfig: Config = {
+    dictionaries: [adjectives, colors],
+    separator: "-",
+    length: 2,
+}
+
+const randomName: string = uniqueNamesGenerator({
+    dictionaries: [adjectives, colors, animals],
+})
+
+const shortName: string = uniqueNamesGenerator(customConfig) // big-donkey
 
 export interface FeedbackData {
     nps: number // net promoter score
@@ -19,18 +34,23 @@ export class FeedbackComponent {
     @HostListener("window:feedback_submitted", ["$event"])
     trackFeedbackSubmitted(event: CustomEvent) {
         const data = event.detail
+        // FullStory.setUserVars({
+        //     displayName: `${uniqueNamesGenerator(customConfig)} ${getEmoji()}`,
+        //     email: generateEmail(),
+        //     totalSpent_real: 14.5, // how much has he spent on in-app purchases so far?
+        // })
         // const { nps, osat, comments } = data
         // const payload = {
         //     uuid: window.localStorage.getItem("_fs_uid"),
         //     nps,
         //     osat,
         //     comments,
-        //     startOfPlayback: FullStory.getCurrentSessionURL(),
+        //     _startOfPlayback: FullStory.getCurrentSessionURL(),
         //     playbackAtThisMomentInTime: FullStory.getCurrentSessionURL(true),
         // }
 
-        console.log("trackSurveyFeedback", data)
         // FullStory.event("feedback_submitted", payload)
+        console.log("trackSurveyFeedback", data)
     }
 
     openDialog(): void {
@@ -43,7 +63,12 @@ export class FeedbackComponent {
                 FullStory.log("warn", "feedback_submitted data not found")
                 return
             }
-            const { nps, osat, comments } = data
+
+            FullStory.setUserVars({
+                displayName: `${uniqueNamesGenerator(customConfig)} ${getEmoji()}`,
+                email: generateEmail(),
+                totalSpent_real: 14.5, // how much has he spent on in-app purchases so far?
+            })
 
             const payload = {
                 fs_uid: window.localStorage.getItem("_fs_uid"),
@@ -51,14 +76,11 @@ export class FeedbackComponent {
                 playbackAtThisMomentInTime: FullStory.getCurrentSessionURL(true),
                 ...data,
             }
-            FullStory.setUserVars({
-                displayName: "Brown Cow 🐄 ",
-                email: "thecowsarein@themeadow.com",
-                totalSpent_real: 14.5, // how much has he spent on in-app purchases so far?
-            })
+
             FullStory.event("feedback_submitted", payload)
             // broadcasts a CustomEvent
             // see https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+            const { nps, osat, comments } = data
             window.dispatchEvent(new CustomEvent("feedback_submitted", { detail: { nps, osat, comments } }))
         })
     }
