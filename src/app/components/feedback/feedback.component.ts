506dfc5316387269048748981a010b7510069e57
PerStirpes <mccarl@asu.edu>
2022-01-17 16:22:10 -0500

    ✨ ✨ ✨

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,15 +1,6 @@
 import { HostListener, Component } from "@angular/core"
 import { MatDialog, MatDialogRef } from "@angular/material/dialog"
 import * as FullStory from "@fullstory/browser"
-import getEmoji from "get-random-emoji"
-import { generateEmail } from "email-generator"
-import { uniqueNamesGenerator, Config, adjectives, colors, animals } from "unique-names-generator"
-
-const customConfig: Config = {
-    dictionaries: [adjectives, colors],
-    separator: "-",
-    length: 2,
-}
 
 export interface FeedbackData {
     nps: number // net promoter score
@@ -29,18 +20,29 @@ export class FeedbackComponent {
     trackFeedbackSubmitted(event: CustomEvent) {
         const data = event.detail
         // FullStory.setUserVars({
-        //     displayName: `${uniqueNamesGenerator(customConfig)} ${getEmoji()}`,
-        //     email: generateEmail(),
+        //     displayName: `Hamburglar 🍔`,
+        //     email: `Hamburglar@mcdonalds.com`
+        //     rewardMbr_bool: true, // are they a member of the reward program?
         //     totalSpent_real: 14.5, // how much has he spent on in-app purchases so far?
         // })
         // const { nps, osat, comments } = data
         // const payload = {
-        //     uuid: window.localStorage.getItem("_fs_uid"),
-        //     nps,
-        //     osat,
-        //     comments,
-        //     _startOfPlayback: FullStory.getCurrentSessionURL(),
-        //     playbackAtThisMomentInTime: FullStory.getCurrentSessionURL(true),
+        //     fsUID_str: window.localStorage.getItem("_fs_uid"),
+        //     // email: `Hamburglar@mcdonalds.com`, //  destination-specific implementation nuance
+        //     referrer: window.document.referrer,
+        //     cartItems_strs: ["pear", "apple", "banana"],
+        //     startOfPlayback_str: FullStory.getCurrentSessionURL(),
+        //     playbackAtThisMomentInTime_str: FullStory.getCurrentSessionURL(true),
+        //     campaignURL_str:
+        //         "utm_source=google&utm_medium=cpc&utm_campaign=972631399&utm_group=52086501761&utm_term=fullstory",
+        //     utm_source_str: "google",
+        //     utm_medium_str: "cpc",
+        //     utm_campaign_str: "972631399",
+        //     utm_group_str: "52086501761",
+        //     utm_term_str: "fullstory",
+        //     nps_int: nps,
+        //     osat_int: osat,
+        //     comments_str: comments,
         // }
 
         // FullStory.event("feedback_submitted", payload)
@@ -57,24 +59,43 @@ export class FeedbackComponent {
                 FullStory.log("warn", "feedback_submitted data not found")
                 return
             }
+            const { nps, osat, comments } = data
 
+            // Who is the user?
+            // You should not use FS.identify for anonymous or guest.
+            // However, you can still attribute custom variables to unidentified users with FS.setUserVars.
             FullStory.setUserVars({
-                displayName: `${uniqueNamesGenerator(customConfig)} ${getEmoji()}`,
-                email: generateEmail(),
-                totalSpent_real: 14.5, // how much has he spent on in-app purchases so far?
+                displayName: `Hamburglar 🍔`,
+                email: `Hamburglar@mcdonalds.com`,
+                rewardMbr_bool: true, // are they a member of the reward program?
+                totalSpent_real: 55.5, // how much have they spent on in-app purchases so far?
             })
 
             const payload = {
-                fs_uid: window.localStorage.getItem("_fs_uid"),
-                _startOfPlayback: FullStory.getCurrentSessionURL(),
-                playbackAtThisMomentInTime: FullStory.getCurrentSessionURL(true),
-                ...data,
+                fsUID_str: window.localStorage.getItem("_fs_uid"),
+                // email: `Hamburglar@mcdonalds.com`, //  destination-specific implementation nuance
+                referrer: window.document.referrer,
+                cartItems_strs: ["pear", "apple", "banana"],
+                startOfPlayback_str: FullStory.getCurrentSessionURL(),
+                playbackAtThisMomentInTime_str: FullStory.getCurrentSessionURL(true),
+                campaignURL_str:
+                    "utm_source=google&utm_medium=cpc&utm_campaign=972631399&utm_group=52086501761&utm_term=fullstory",
+                utm_source_str: "google",
+                utm_medium_str: "cpc",
+                utm_campaign_str: "972631399",
+                utm_group_str: "52086501761",
+                utm_term_str: "fullstory",
+                nps_int: nps,
+                osat_int: osat,
+                comments_str: comments,
             }
 
+            // Events("feedback_submitted") are trackable actions
+            // properties ("payload") is the data about the event that is sent with each event.
             FullStory.event("feedback_submitted", payload)
             // broadcasts a CustomEvent
             // see https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
-            const { nps, osat, comments } = data
+
             window.dispatchEvent(new CustomEvent("feedback_submitted", { detail: { nps, osat, comments } }))
         })
     }
