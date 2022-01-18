f10a1080085911b8aeb0926982c780ea26d04faf
PerStirpes <mccarl@asu.edu>
2022-01-18 10:44:05 -0500

    Add rageclick

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,34 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAnalyticsModule, ScreenTrackingService } from '@angular/fire/analytics';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { HttpClientModule } from "@angular/common/http"
+import { NgModule } from "@angular/core"
+import { FormsModule } from "@angular/forms"
+import { AngularFireModule } from "@angular/fire"
+import { AngularFireAnalyticsModule, ScreenTrackingService } from "@angular/fire/analytics"
+import { AngularFireAuthModule } from "@angular/fire/auth"
+import { BrowserModule } from "@angular/platform-browser"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { FlexLayoutModule } from "@angular/flex-layout"
+import { MatBadgeModule } from "@angular/material/badge"
+import { MatButtonModule } from "@angular/material/button"
+import { MatCardModule } from "@angular/material/card"
+import { MatDialogModule } from "@angular/material/dialog"
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatGridListModule } from "@angular/material/grid-list"
+import { MatIconModule } from "@angular/material/icon"
+import { MatInputModule } from "@angular/material/input"
+import { MatListModule } from "@angular/material/list"
+import { MatMenuModule } from "@angular/material/menu"
+import { MatRadioModule } from "@angular/material/radio"
+import { MatSelectModule } from "@angular/material/select"
+import { MatSnackBarModule } from "@angular/material/snack-bar"
+import { MatTableModule } from "@angular/material/table"
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { MatTooltipModule } from "@angular/material/tooltip"
 
-import { environment } from 'src/environments/environment';
+import { environment } from "src/environments/environment"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
 import {
-  CartComponent, CartButtonComponent, CheckoutComponent, FeedbackComponent, FeedbackDialog,
-  ProductsComponent, ThankYouComponent
-} from './components';
-import { CartService, ProductService } from './services';
-import { LoginComponent } from './components/login/login.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
     CartComponent,
     CartButtonComponent,
     CheckoutComponent,
@@ -45,40 +36,50 @@ import { LoginComponent } from './components/login/login.component';
     FeedbackDialog,
     ProductsComponent,
     ThankYouComponent,
-    LoginComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAnalyticsModule,
-    AngularFireAuthModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FlexLayoutModule,
-    HttpClientModule,
-    MatBadgeModule,
-    MatButtonModule,
-    MatCardModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatSnackBarModule,
-    MatTableModule,
-    MatToolbarModule,
-    MatTooltipModule,
-  ],
-  providers: [
-    CartService,
-    ProductService,
-    ScreenTrackingService,
-  ],
-  bootstrap: [AppComponent]
+} from "./components"
+import { CartService, ProductService } from "./services"
+import { LoginComponent } from "./components/login/login.component"
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        CartComponent,
+        CartButtonComponent,
+        CheckoutComponent,
+        FeedbackComponent,
+        FeedbackDialog,
+        ProductsComponent,
+        ThankYouComponent,
+        LoginComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAnalyticsModule,
+        AngularFireAuthModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        HttpClientModule,
+        MatBadgeModule,
+        MatButtonModule,
+        MatCardModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSnackBarModule,
+        MatTableModule,
+        MatToolbarModule,
+        MatTooltipModule,
+    ],
+    providers: [CartService, ProductService, ScreenTrackingService, FeedbackComponent],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,74 +1,81 @@
-import { Component, OnInit } from '@angular/core';
-import { MediaChange, MediaObserver } from '@angular/flex-layout';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { ProductService, CartService } from 'src/app/services';
-import { Product } from 'src/app/models';
+import { HostListener, Component, OnInit } from "@angular/core"
+import { MediaChange, MediaObserver } from "@angular/flex-layout"
+import { MatSnackBar } from "@angular/material/snack-bar"
+import { ProductService, CartService } from "src/app/services"
+import { Product } from "src/app/models"
+import { FeedbackComponent } from "../feedback/feedback.component"
 
 /**
  * Breakpoint aliases (e.g. 'sm') to number of grid-list columns.
  */
 interface BreakpointMap {
-  [name: string]: number
+    [name: string]: number
 }
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+    selector: "app-products",
+    templateUrl: "./products.component.html",
+    styleUrls: ["./products.component.scss"],
 })
 export class ProductsComponent implements OnInit {
+    // TODO (van) a better approach is min/max width based on the product's image size
+    private columnBreakpoints: BreakpointMap = {
+        default: 3,
+        xs: 1,
+        sm: 2,
+        md: 3,
+        lg: 4,
+        xl: 5,
+    }
 
-  // TODO (van) a better approach is min/max width based on the product's image size
-  private columnBreakpoints: BreakpointMap = {
-    'default': 3,
-    'xs': 1,
-    'sm': 2,
-    'md': 3,
-    'lg': 4,
-    'xl': 5,
-  };
+    numColumns = this.columnBreakpoints["default"]
+    products: Product[] = []
 
-  numColumns = this.columnBreakpoints['default'];
-  products: Product[] = [];
+    constructor(
+        private cartService: CartService,
+        private mediaObserver: MediaObserver,
+        private productService: ProductService,
+        private snackbar: MatSnackBar,
+        private feedBack: FeedbackComponent,
+    ) {}
 
-  constructor(
-    private cartService: CartService,
-    private mediaObserver: MediaObserver,
-    private productService: ProductService,
-    private snackbar: MatSnackBar,
-  ) { }
+    ngOnInit() {
+        this.observeBreakpoints()
+        this.buildProductList()
+    }
 
-  ngOnInit() {
-    this.observeBreakpoints();
-    this.buildProductList();
-  }
+    @HostListener("window:fullstory/rageclick", ["$event"])
+    trackRageClick(event: CustomEvent) {
+        console.log("trackRageClick", event)
+        this.feedBack.openDialog()
+    }
 
-  /**
-   * Adds a product to the user's shopping cart.
-   * A snackbar will appear showing the product's title that was added.
-   * @param product the Product to be added
-   */
-  addToCart(product: Product) {
-    const { id } = product;
-    this.cartService.addItem(id);
-    this.snackbar.open(`Added ${product.title} to your cart`, '', { duration: 2000 });
-  }
+    /**
+     * Adds a product to the user's shopping cart.
+     * A snackbar will appear showing the product's title that was added.
+     * @param product the Product to be added
+     */
+    addToCart(product: Product) {
+        const { id } = product
+        this.cartService.addItem(id)
+        this.snackbar.open(`Added ${product.title} to your cart`, "", { duration: 2000 })
+    }
 
-  /**
-   * Observes for breakpoint changes and updates the number of columns.
-   * See `columnBreakpoints` for breakpoint-to-columns mapping.
-   */
-  private observeBreakpoints(): void {
-    this.mediaObserver.media$.subscribe((change: MediaChange) => {
-      const n = this.columnBreakpoints[change.mqAlias];
-      this.numColumns = n > 0 ? n : this.columnBreakpoints['default'];
-    });
-  }
+    /**
+     * Observes for breakpoint changes and updates the number of columns.
+     * See `columnBreakpoints` for breakpoint-to-columns mapping.
+     */
+    private observeBreakpoints(): void {
+        this.mediaObserver.media$.subscribe((change: MediaChange) => {
+            const n = this.columnBreakpoints[change.mqAlias]
+            this.numColumns = n > 0 ? n : this.columnBreakpoints["default"]
+        })
+    }
 
-  /**
-   * Builds a list of products from the catalog.
-   */
-  buildProductList(): void {
-    this.productService.getProducts().subscribe(products => this.products = products);
-  }
+    /**
+     * Builds a list of products from the catalog.
+     */
+    buildProductList(): void {
+        this.productService.getProducts().subscribe((products) => (this.products = products))
+    }
 }
